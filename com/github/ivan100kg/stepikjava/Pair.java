f2fe5206804b9d447ae79026290f187d91344b6b
Ivan B <ivar110kg@gmail.com>
2022-04-05 15:24:36 +0400

    do homework 6.1

---
+++ com/github/ivan100kg/stepikjava/Pair.java
@@ -0,0 +1,61 @@
+package com.github.ivan100kg.stepikjava;
+
+import java.util.Objects;
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    private Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
+        return new Pair<>(first, second);
+    }
+
+    T1 getFirst() {
+        return first;
+    }
+
+    T2 getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+
+}
+
+class TestPair {
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst();
+        String s = pair.getSecond();
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+    }
+}
