cc8ee62886718ada6a315ff29a4818140a4358e7
Ivan B <ivar110kg@gmail.com>
2022-04-05 10:36:00 +0400

    do homework 4.1

---
+++ com/github/ivan100kg/stepikjava/Hw41.java
@@ -0,0 +1,24 @@
+package com.github.ivan100kg.stepikjava;
+
+public class Hw41 {
+}
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+        Thread current = Thread.currentThread();
+        StackTraceElement[] methods = current.getStackTrace();
+        if (methods.length > 3)
+            return methods[3].getClassName() + "#" + methods[3].getMethodName();
+        return null;
+    }
+}
+
